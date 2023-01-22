b5b54eeaf99fef8fce573a5bda0d300aa465afd8
Parfyanovich <parfyanovichpavel@gmail.com>
2023-01-22 14:34:54 +0300

    docs: create brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let str1;
+  let bc = bracketsConfig.flat().join('').match(/.{2}/g);
+  for (let i = 0; i < bc.length; i++) {
+    str1 = str;
+    str = str.replaceAll(bc[i], '');
+    if (str1 !== str) i=-1;
+    if (str.length === 0) return true;
+  }
+  return false;
 }
